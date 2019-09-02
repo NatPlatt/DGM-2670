a0882d562f9f6ab2a0a04e2928a09cc51a8f90a4
NatPlatt <n.zollinger26@hotmail.com>
2019-09-02 15:03:30 -0600

    added enemies, edited player script

--- DGM 2670 Game/Assets/Scripts/PlayerController.cs
+++ DGM 2670 Game/Assets/Scripts/PlayerController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
-
+    public float speed = 20.0f;
+    public float turnSpeed = 45.0f;
+    public float horizontalInput;
+    public float forwardInput; 
+    
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 5);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
