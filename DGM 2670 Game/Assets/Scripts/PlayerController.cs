12471f29fab78307ff1cd6c7a038c61e0a6228c7
NatPlatt <n.zollinger26@hotmail.com>
2019-09-03 10:27:47 -0600

    tested scripts

--- DGM 2670 Game/Assets/Scripts/PlayerController.cs
+++ DGM 2670 Game/Assets/Scripts/PlayerController.cs
@@ -7,11 +7,15 @@ public class PlayerController : MonoBehaviour
     public float speed = 20.0f;
     public float turnSpeed = 45.0f;
     public float horizontalInput;
-    public float forwardInput; 
+    public float forwardInput;
+    public float xRange = 7;
     
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x > xRange){ transform.position = new Vector3(xRange, transform.position.y, transform.position.z);}
+        if (transform.position.x < -xRange){transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);}
+        
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
         
