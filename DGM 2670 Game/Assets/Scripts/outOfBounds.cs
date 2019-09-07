cee13e17ce7e35858d4e14a592efa7475d45ce13
NatPlatt <n.zollinger26@hotmail.com>
2019-09-07 14:33:46 -0600

    added out of bounds script
    script is on enemy capsules

---
+++ DGM 2670 Game/Assets/Scripts/outOfBounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outOfBounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private float lowerBound = -2;
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z <= lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
