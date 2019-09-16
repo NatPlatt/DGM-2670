4e07ad7a3bfc6057be101a2364667e8b93558095
NatPlatt <n.zollinger26@hotmail.com>
2019-09-16 14:07:37 -0600

    cleaning up script

--- DGM 2670 Game/Assets/Scripts/PowerBooster.cs
+++ DGM 2670 Game/Assets/Scripts/PowerBooster.cs
@@ -6,78 +6,12 @@ using UnityEngine;
 
 public class PowerBooster : MonoBehaviour
 {
-    public float speed = 1.0f;
-   public enum PowerStates
+    public FloatData speed;
+
+    private void Update()
    {
-      Normal,
-      Fast,
-      Slow,
-      Stop
-   }
-
-   public PowerStates powerState = PowerStates.Normal;
-
-   private void OnTriggerEnter(Collider other)
-   {
-       if (other.name == "fast")
-       {
-           powerState = PowerStates.Fast;
-       }
-
-       if (other.name == "slow")
-       {
-           powerState = PowerStates.Slow;
-       }
-
-       if (other.name == "normal")
-       {
-           powerState = PowerStates.Normal;
-       }
-
-       if (other.name == "stop")
-       {
-           powerState = PowerStates.Normal;
-       }
-   }
-
-   private void Update()
-   {
-       switch (powerState)
-       {
-           case PowerStates.Normal:
-               Normal();
-               break;
-           case PowerStates.Fast:
-               Fast();
-               break;
-           case PowerStates.Slow:
-               Slow();
-               break;
-           case PowerStates.Stop:
-               Stop();
-               break;
-       }
-       var speedVector = new Vector3(speed, 0,0 )*Time.deltaTime;
+       var speedVector = new Vector3(speed.value, 0,0 )*Time.deltaTime;
        transform.Translate(speedVector);
    }
-    
-   public void Normal()
-   {
-       speed = 1f;
-   }
-
-   public void Fast()
-   {
-       speed = 5f;
-   }
-
-   public void Slow()
-   {
-       speed = 0.2f;
-   }
-
-   public void Stop()
-   {
-       speed = 0;
-   }
+  
 }
