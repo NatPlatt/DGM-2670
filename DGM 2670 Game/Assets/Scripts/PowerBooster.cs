c15d2558597145756b1d1deb66730b9ab96ed764
NatPlatt <n.zollinger26@hotmail.com>
2019-09-16 13:20:00 -0600

    long (dumb) way of writing powerBoost code
    with individual functions and switch statements

---
+++ DGM 2670 Game/Assets/Scripts/PowerBooster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerBooster : MonoBehaviour
+{
+    public float speed = 1.0f;
+   public enum PowerStates
+   {
+      Normal,
+      Fast,
+      Slow,
+      Stop
+   }
+
+   public PowerStates powerState = PowerStates.Normal;
+
+   public void Normal()
+   {
+       speed = 1f;
+   }
+
+   public void Fast()
+   {
+       speed = 5f;
+   }
+
+   public void Slow()
+   {
+       speed = 0.2f;
+   }
+
+   public void Stop()
+   {
+       speed = 0;
+   }
+}
