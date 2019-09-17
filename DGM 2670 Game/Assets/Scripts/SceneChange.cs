37a49ca64e3ad36b5eb8992d3136402f67780b41
NatPlatt <n.zollinger26@hotmail.com>
2019-09-17 11:59:34 -0600

    changed scene script
    updated application.loadscene to scenemanager.loadscene

--- DGM 2670 Game/Assets/Scripts/SceneChange.cs
+++ DGM 2670 Game/Assets/Scripts/SceneChange.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
    public void ChangeScene(string scenename)
    {
-      Application.LoadLevel (scenename);
+      SceneManager.LoadScene(scenename);
    }
 }
