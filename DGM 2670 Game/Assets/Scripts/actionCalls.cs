8cffd8b6681a5e2658bffbe93f1d7b27929e7c3a
NatPlatt <n.zollinger26@hotmail.com>
2019-09-18 14:01:38 -0600

    changing script to use unity actions

--- DGM 2670 Game/Assets/Scripts/actionCalls.cs
+++ DGM 2670 Game/Assets/Scripts/actionCalls.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class actionCalls : MonoBehaviour
 {
     public float speed = 1f, rotateSpeed = 10f, scaleSpeed = 0.2f;
     private Vector3 location, rotations, scales;
-    
+    private UnityAction TransformAction;
+
+    private void Start()
+    {
+        TransformAction = OnMove;
+    }
+
     public enum TransformStates
     {
         Move,
@@ -23,32 +30,22 @@ public class actionCalls : MonoBehaviour
         {
             case TransformStates.Move:
                 transformState = TransformStates.Rotate;
+                TransformAction = OnRotate;
                 break;
             case TransformStates.Rotate:
                 transformState = TransformStates.Scale;
+                TransformAction = OnScale;
                 break;
             case TransformStates.Scale:
                 transformState = TransformStates.Move;
+                TransformAction = OnMove;
                 break;
         }
     }
 
     void Update()
     {
-        switch (transformState)
-        {
-            case TransformStates.Move:
-                OnMove();
-                break;
-            case TransformStates.Rotate:
-                OnRotate();
-                break;
-            case TransformStates.Scale:
-                OnScale();
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
+        TransformAction();
     }
     public void OnMove()
     {
