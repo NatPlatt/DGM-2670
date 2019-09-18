36906f332dbf808abff8a6f101c68b5fb8c27204
NatPlatt <n.zollinger26@hotmail.com>
2019-09-18 13:27:35 -0600

    wrote actionCalls script

---
+++ DGM 2670 Game/Assets/Scripts/actionCalls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class actionCalls : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotateSpeed = 10f;
+    private Vector3 location;
+    private Vector3 rotations;
+    
+    void Update()
+    {
+        OnMove();
+        OnRotate();
+    }
+    public void OnMove()
+    {
+        location.x = speed * Time.deltaTime;
+        transform.Translate(location);
+    }
+
+    public void OnRotate()
+    {
+        rotations.y = rotateSpeed * Time.deltaTime;
+        transform.Rotate(rotations);
+    }
+}
