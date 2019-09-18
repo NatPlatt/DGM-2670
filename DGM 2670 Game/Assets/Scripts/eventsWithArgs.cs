71880d128ba9d868c642fe97bf55ad656e713eef
NatPlatt <n.zollinger26@hotmail.com>
2019-09-18 14:42:48 -0600

    saved unity project

---
+++ DGM 2670 Game/Assets/Scripts/eventsWithArgs.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class eventsWithArgs : MonoBehaviour
+{
+    public UnityEvent<float> eventWithFloat;
+    
+    void Start()
+    {
+        
+        eventWithFloat.AddListener(PassFloat);
+    }
+
+    public void PassFloat(float number)
+    {
+        print(number);
+    }
+    
+    void Update()
+    {
+        eventWithFloat.Invoke(10);
+    }
+}
