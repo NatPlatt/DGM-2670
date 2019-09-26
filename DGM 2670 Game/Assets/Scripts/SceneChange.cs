d72e8a21189fa38760c76e951c8b02158d3a63fb
NatPlatt <n.zollinger26@hotmail.com>
2019-09-26 09:24:51 -0600

    adding fade in and fade out scripts and animation

--- DGM 2670 Game/Assets/Scripts/SceneChange.cs
+++ DGM 2670 Game/Assets/Scripts/SceneChange.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
-   public void ChangeScene(string scenename)
+
+    public Animator animator;
+
+    /*private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel("Page"+1);
+        }
+    }
+    */
+
+    public void ChangeScene(string scenename)
    {
-      SceneManager.LoadScene(scenename);
+       SceneManager.LoadScene(scenename);
    }
+
+    public void FadeToLevel(string scenename)
+    {
+        animator.SetTrigger("ButtonClicked");
+    }
 }
