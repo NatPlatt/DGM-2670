36f91d3944ce5f11b19ae243b605f2def76e49b6
NatPlatt <n.zollinger26@hotmail.com>
2019-09-28 11:18:02 -0600

    added drag controller script

---
+++ DGM 2670 Game/Assets/Scripts/DragController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragController : MonoBehaviour, IBeginDragHandler,IDragHandler,IEndDragHandler
+{
+    public static GameObject myDragSprite;
+    private Vector3 startPosition;
+    private float zDistance;
+    private Vector3 touchOffset;
+    
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+}
