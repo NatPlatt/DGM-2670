ce5e5e2264c0504b20cce4196677c79c930b53f1
NatPlatt <n.zollinger26@hotmail.com>
2019-10-02 13:13:36 -0600

    made coroutine script

---
+++ DGM 2670 Game/Assets/Scripts/Coroutines.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coroutines : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(2);
+        print("ran");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
