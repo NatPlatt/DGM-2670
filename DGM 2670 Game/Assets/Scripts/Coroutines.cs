ccf21a81524ce4ce9ea76e152da38e6dd1145bd4
NatPlatt <n.zollinger26@hotmail.com>
2019-10-02 13:25:19 -0600

    edit start method

--- DGM 2670 Game/Assets/Scripts/Coroutines.cs
+++ DGM 2670 Game/Assets/Scripts/Coroutines.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class Coroutines : MonoBehaviour
 {
-    public Vector3 location;
+    private int jumpCount;
+    private int jumpCountMax = 10;
+    
+
     IEnumerator Start()
     {
-        yield return new WaitForSeconds(2);
-        print("ran");
-        transform.position += location;
+        //yield return new WaitForSeconds(2);
+       // print("ran");
+        Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
+        while (jumpCount < jumpCountMax)
+        {
+            yield return new WaitForSeconds(2);
+            transform.position += Vector3.forward;
+            jumpCount++;
+        }
     }
 
     // Update is called once per frame
