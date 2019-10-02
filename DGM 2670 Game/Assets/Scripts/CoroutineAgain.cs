c5bba5fa55f85b7ca3a233324d60657a466e100b
NatPlatt <n.zollinger26@hotmail.com>
2019-10-02 13:59:19 -0600

    made another coroutine script

---
+++ DGM 2670 Game/Assets/Scripts/CoroutineAgain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CoroutineAgain : MonoBehaviour
+{
+   private WaitForSeconds wfs;
+   public float holdTime = 0.1f;
+   public bool canRun = true;
+   public UnityEvent OnRunEvent;
+   
+   private void Awake()
+   {
+      wfs = new WaitForSeconds(holdTime);
+   }
+
+   private IEnumerator OnStart()
+   {
+      while (canRun)
+      {
+         print("Run");
+         OnRunEvent.Invoke();
+         yield return wfs;
+      } 
+   }
+}
