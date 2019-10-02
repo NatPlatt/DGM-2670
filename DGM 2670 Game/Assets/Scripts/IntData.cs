b6c0f955dba908d5dfc702e8572cc77c52c28f69
NatPlatt <n.zollinger26@hotmail.com>
2019-10-02 14:11:01 -0600

    added to IntData scriptable object

--- DGM 2670 Game/Assets/Scripts/IntData.cs
+++ DGM 2670 Game/Assets/Scripts/IntData.cs
@@ -7,12 +7,18 @@ public class IntData : ScriptableObject
 {
     public int value = 0;
     public int maxValue = 2;
+    
 
     public void UpdateToMaxValue()
     {
         value = maxValue;
     }
 
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+    
     public void UpdateToNumber(int number)
     {
         value = number;
