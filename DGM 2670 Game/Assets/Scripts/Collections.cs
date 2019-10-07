33b7a9eb5c281d9afcded26b57d886c6514396c1
NatPlatt <n.zollinger26@hotmail.com>
2019-10-07 14:28:03 -0600

    added collections script

---
+++ DGM 2670 Game/Assets/Scripts/Collections.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Collections : ScriptableObject
+{
+    public List<Object> ObjectList;
+    public List<FloatData> FloatDatas;
+    public List<GameObject> GameObjects;
+
+
+    public void FindObjectType(Object obj)
+    {
+        foreach (var currentObj in ObjectList)
+        {
+            if (currentObj == obj)
+            {
+                //Do work
+                //could instantiate, add to collection, call something
+            }
+        }
+    }
+
+    public void AddToList(Object obj)
+    {
+        ObjectList.Add(obj);
+    }
+
+    public void RemoveFromList(Object obj)
+    {
+       foreach (var currentObj in ObjectList)
+               {
+                   if (currentObj == obj)
+                   {
+                       ObjectList.Remove(obj);
+                   }
+               }
+    }
+    
+}
