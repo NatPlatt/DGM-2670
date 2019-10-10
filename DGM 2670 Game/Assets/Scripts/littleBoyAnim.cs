7660b1bb3f1e057d801573e35c02edc57f25e158
NatPlatt <n.zollinger26@hotmail.com>
2019-10-10 10:12:01 -0600

    wrote LittleBoyAnim script
    to use unity actions and events to control animations

--- DGM 2670 Game/Assets/Scripts/littleBoyAnim.cs
+++ DGM 2670 Game/Assets/Scripts/littleBoyAnim.cs
@@ -1,18 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class littleBoyAnim : MonoBehaviour
+public class LittleBoyAnim : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameAction gameActionObj;
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        gameActionObj.Raise();
     }
 }
