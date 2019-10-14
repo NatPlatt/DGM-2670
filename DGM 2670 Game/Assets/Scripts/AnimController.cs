d5a93786ed33cd23c6d0373dcf94f2501de0d7c4
NatPlatt <n.zollinger26@hotmail.com>
2019-10-14 14:11:23 -0600

    made multiple scripts
    MoveCharacter script, MouseEvents script, and Debugger script.

---
+++ DGM 2670 Game/Assets/Scripts/AnimController.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AnimController : ScriptableObject
+{
+    
+    
+    public void AnimCall(Animator animator)
+    {
+        animator.SetTrigger(name);
+    }
+}
