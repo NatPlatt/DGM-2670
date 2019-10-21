4ae37dc0c5dfac1d3e8ba47f8ccbbc764806b0b5
NatPlatt <n.zollinger26@hotmail.com>
2019-10-21 13:42:05 -0600

    wrote powerup script and IRun script

---
+++ DGM 2670 Game/Assets/Scripts/Powerup.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Powerup : ScriptableObject, IRun
+{
+    public void Run()
+    {
+        Debug.Log("powerup");
+    }
+}
