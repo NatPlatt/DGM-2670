7333e76a0d03fe17a7bae64b8478df88848e1c63
NatPlatt <n.zollinger26@hotmail.com>
2019-10-23 13:20:42 -0600

    had error with button on page one
    after trying a lot of different things, I ended up deleting it and making it again

--- DGM 2670 Game/Assets/Scripts/SceneChange.cs
+++ DGM 2670 Game/Assets/Scripts/SceneChange.cs
@@ -1,30 +1,13 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class SceneChange : MonoBehaviour
+[CreateAssetMenu]
+public class SceneChange : ScriptableObject
 {
 
-    public Animator animator;
-
-    /*private void Update()
+    public void LoadScene(Object sceneName)
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            FadeToLevel("Page"+1);
-        }
-    }
-    */
-
-    public void ChangeScene(string scenename)
-   {
-       SceneManager.LoadScene(scenename);
-   }
-
-    public void FadeToLevel(string scenename)
-    {
-        animator.SetTrigger("ButtonClicked");
+        var newName = sceneName.name;
+        SceneManager.LoadScene(newName);
     }
 }
