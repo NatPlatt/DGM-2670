7015cb02ba4ab9cc57d5f60d0f5d8ea3d04c343d
NatPlatt <n.zollinger26@hotmail.com>
2019-10-29 21:41:47 -0600

    still trying to find a good way to animate on click

---
+++ DGM 2670 Game/Assets/Scripts/clickAnims.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickAnims : MonoBehaviour
+{
+    public Animator animator;
+    public ScriptableObject scriptObj;
+    
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+   
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Active");
+        }
+    }
+}
