82bf1a63b24f19ab2ef3109696dd33df7bde4f83
NatPlatt <n.zollinger26@hotmail.com>
2019-11-18 14:50:46 -0700

    edited scripts to get tower defense movement to work

---
+++ DGM 2670 Game/Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Mover : ScriptableObject
+{
+    public GameObject player;
+    public int speed = 1;
+    
+    public void MoveIt()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0 , (Input.GetAxis("Vertical") )) ;
+
+        player.transform.position = Movement * speed * Time.deltaTime;
+    }
+}
