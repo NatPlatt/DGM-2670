e14ad7a0bd5604ac524f92d9d922b45f042bd025
NatPlatt <n.zollinger26@hotmail.com>
2019-11-19 18:09:32 -0700

    added draggable script for items

--- DGM 2670 Game/Assets/Scripts/Draggable.cs
+++ DGM 2670 Game/Assets/Scripts/Draggable.cs
@@ -5,5 +5,34 @@ using UnityEngine;
 
 public class Draggable : MonoBehaviour
 {
+   private Vector3 offsetPos;
+   private float mouseZpos;
    
+   public Camera camera;
+   
+   public bool canDrag { get; set; }
+
+   private void Start()
+   {
+      camera = Camera.main;
+   }
+
+   private void OnMouseDown()
+   {
+      canDrag = true;
+      mouseZpos = camera.WorldToScreenPoint(gameObject.transform.position).z;
+      offsetPos = gameObject.transform.position - GetMouseWorldPos();
+   }
+
+   private Vector3 GetMouseWorldPos()
+   {
+      Vector3 mousePoint = Input.mousePosition;
+      mousePoint.z = mouseZpos;
+      return camera.ScreenToWorldPoint(mousePoint);
+   }
+
+   private void OnMouseDrag()
+   {
+      transform.position = GetMouseWorldPos() + offsetPos;
+   }
 }
