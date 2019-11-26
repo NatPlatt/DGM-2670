12845cff5d766d6ef6ff74ad2d7a562e35278e97
NatPlatt <n.zollinger26@hotmail.com>
2019-11-26 10:42:29 -0700

    writing obstacle script

---
+++ DGM 2670 Game/Assets/Scripts/obstacles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacles : MonoBehaviour
+{
+    public Animation obstacleAnim;
+    public Animator animController;
+    public void OnTriggerEnter(Collider other)
+    {
+        //obstacleAnim.Play();
+        var animControllerEnabled = animController.enabled;
+    }
+}
