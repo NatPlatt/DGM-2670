1a55bd3f2f908617339eb34e3a0bab952c2c9bc1
NatPlatt <n.zollinger26@hotmail.com>
2019-12-09 10:26:01 -0700

    fixed music so it'll play from scene to scene

--- DGM 2670 Game/Assets/Scripts/audioController.cs
+++ DGM 2670 Game/Assets/Scripts/audioController.cs
@@ -1,8 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class audioController : MonoBehaviour
 {
-    
+    public GameAction Action;
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        Action.action += StopMusic;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlayMusic()
+    {
+        if (audioSource.isPlaying) return;
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
 }
